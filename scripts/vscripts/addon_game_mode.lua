970d8a24a52fbae4c51363f294c9d6b01acf2a54
Chuzzwazza <angus.chippendale@live.com>
2015-04-10 00:51:13 +1000

    added new gamerule/gamemode settings

--- scripts/vscripts/addon_game_mode.lua
+++ scripts/vscripts/addon_game_mode.lua
@@ -1,95 +1,89 @@
---[[
-Dota PvP game mode
-]]
-
-print( "HAM game mode loaded." )
+print("Going ham.")
 
 if HAM == nil then
-	HAM = {}
-	HAM.__index = HAM
+	HAM = class({})
 end
 
 XP_PER_LEVEL_TABLE = {
-     0, -- 1
-  200, -- 2
-  400, -- 3
-  900, -- 4
-  1400, -- 5
- 2000, -- 6
- 2600, -- 7
- 3200, -- 8
- 4400, -- 9
- 5400, -- 10
- 6000, -- 11
- 8200, -- 12
- 9000, -- 13
- 10400, -- 14
- 11900, -- 15
- 13500, -- 16
- 15200, -- 17
- 17000, -- 18
- 18900, -- 19
-20900, -- 20
-23000, -- 21
-25200, -- 22
-27500, -- 23
-29900, -- 24
-32400,  -- 25
-35000, -- 26
-37700, -- 27
-40500, -- 28
-43400, -- 29
-46400, -- 30
-49500, -- 31
-52700, -- 32
-56000, -- 33
-59400, -- 34
-62900 -- 35
+	0, -- 1
+	200, -- 2
+	400, -- 3
+	900, -- 4
+	1400, -- 5
+	2000, -- 6
+	2600, -- 7
+	3200, -- 8
+	4400, -- 9
+	5400, -- 10
+	6000, -- 11
+	8200, -- 12
+	9000, -- 13
+	10400, -- 14
+	11900, -- 15
+	13500, -- 16
+	15200, -- 17
+	17000, -- 18
+	18900, -- 19
+	20900, -- 20
+	23000, -- 21
+	25200, -- 22
+	27500, -- 23
+	29900, -- 24
+	32400, -- 25
+	35000, -- 26
+	37700, -- 27
+	40500, -- 28
+	43400, -- 29
+	46400, -- 30
+	49500, -- 31
+	52700, -- 32
+	56000, -- 33
+	59400, -- 34
+	62900 -- 35
 }
 
 function Activate()
-    GameRules.HAM = HAM()
-    GameRules.HAM:InitGameMode()
+    GameRules.AddonTemplate = HAM()
+    GameRules.AddonTemplate:InitGameMode()
 end
 
 function HAM:InitGameMode()
 	local GameMode = GameRules:GetGameModeEntity()
 
+	-- Set thinker
+	GameMode:SetThink("OnThink", self, "GlobalThink", 1)
+
+	-- Set listener
+	-- ListenToGameEvent("entity_killed", Dynamic_Wrap(HAM, "OnEntityKilled"), self)
+
+	-- Enable backdoor protection
+	GameMode:SetTowerBackdoorProtectionEnabled(true)
+
 	-- Enable custom buyback cooldown
 	GameMode:SetCustomBuybackCooldownEnabled(true)
-	ListenToGameEvent('player_connect_full', SetBuyback, self)
-
-	-- Enable the standard Dota PvP game rules
-	GameRules:GetGameModeEntity():SetTowerBackdoorProtectionEnabled( true )
-
-	-- Register Think
-	GameMode:SetContextThink( "HAM:GameThink", function() return self:GameThink() end, 0.25 )
 
 	-- Set custom levels/experience
 	GameMode:SetCustomXPRequiredToReachNextLevel(XP_PER_LEVEL_TABLE)
 	GameMode:SetCustomHeroMaxLevel(35)
 	GameMode:SetUseCustomHeroLevels(true)
 
-	-- Set custom gold income
+	-- Set gold income
 	GameRules:SetGoldTickTime(0.12)
 
-	-- Enable same hero
+	-- Enable same hero selection
 	GameRules:SetSameHeroSelectionEnabled(true)
+	
+	-- Set hero selection time
+	GameRules:SetHeroSelectionTime(90)
 end
 
-function SetBuyback(keys)
-	local GameMode = GameRules:GetGameModeEntity()
-	local entIndex = keys.index+1
-	local player = EntIndexToHScript(entIndex)
-	PlayerResource:SetCustomBuybackCooldown(player, 252)
+function HAM:OnThink()
+	return 1
 end
 
-function HAM:GameThink()
-	return 0.25
-end
-
--- bool HaveAllPlayersJoined()
--- bool IsValidPlayerID(int playerID) 
--- int GetPlayerOwnerID() 
--- int GetPlayerID() 
--- void SetHeroSelectionTime(float time)
+-- function HAM:OnEntityKilled(keys)
+-- 	local killedUnit = EntIndexToHScript(keys.entindex_killed)
+-- 	if killedUnit:IsRealHero() then
+-- 		PlayerResource:SetCustomBuybackCooldown(killedUnit:GetPlayerID(), 252)
+--     end
+-- end
